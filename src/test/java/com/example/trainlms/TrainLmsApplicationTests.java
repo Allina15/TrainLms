ee4454d3423a97dc3d4679430313376f866e7c9d
Alina <aakhunova02@gmail.com>
2023-11-28 16:24:01 +0600

    Initial commit

---
+++ src/test/java/com/example/trainlms/TrainLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainlms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainLmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
